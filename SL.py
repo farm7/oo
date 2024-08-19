283f646c4458ef55faf2f866533e5134f961fc86
farm7 <jorgedcianca@gmail.com>
2024-08-18 21:17:11 -0400

    Update SL.py

--- SL.py
+++ SL.py
@@ -1,5 +1,16 @@
 import streamlit as st
 import pandas as pd
+from pathlib import Path
+
+import appdirs as ad
+
+CACHE_DIR = ".cache"
+
+# Force appdirs to say that the cache dir is .cache
+ad.user_cache_dir = lambda *args: CACHE_DIR
+
+# Create the cache dir if it doesn't exist
+Path(CACHE_DIR).mkdir(exist_ok=True)
 import yfinance as yf
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
